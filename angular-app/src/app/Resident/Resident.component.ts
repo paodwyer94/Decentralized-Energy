2741b110c77f3ab22efcbb030bff001814fb31e6
paodwyer94 <40065585+paodwyer94@users.noreply.github.com>
2018-07-04 21:35:07 +0100

    Add files via upload

--- angular-app/src/app/Resident/Resident.component.ts
+++ angular-app/src/app/Resident/Resident.component.ts
@@ -143,14 +143,14 @@ export class ResidentComponent {
       .then(() => {           
         this.errorMessage = null;
         this.myForm.setValue({
-            "residentID":CEng,
-            "firstName":Civil,
-            "lastName":Engineering,
-            "coinsValue":100,
-            "energyValue":10,
-            "energyUnits":1,
-            "cashValue":1,
-            "cashCurrency":1
+            "residentID":null,
+            "firstName":null,
+            "lastName":null,
+            "coinsValue":null,
+            "energyValue":null,
+            "energyUnits":null,
+            "cashValue":null,
+            "cashCurrency":null
         });
       })
     .catch((error) => {
